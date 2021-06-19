f186de74fc8557573a31adef45ce479aa2e5898d
Reza Andriansyah <rezaxxxx02@gmail.com>
2021-06-19 19:18:24 +0700

    setup package and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge:{
+    enabled: true,
+    content: [
+      "./pages/**/*.jsx",
+      "./components/**/*.jsx"
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
